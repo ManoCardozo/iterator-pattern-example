e332880bb18fdc1e939576578b3870ab4dc913b2
Mano Cardozo <manasses.cardozo@gmail.com>
2020-08-29 15:23:21 +1200

    Create basic project structure

---
+++ CardDeck.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardDeck.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
